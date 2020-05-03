0c4cffa6687ac24f5fe010e7634e3049e71207bb
Misha Hrokhola <m.hrokhola@tenantcloud.com>
2020-05-03 11:53:10 +0300

    blocked btn after click

--- js/index.js
+++ js/index.js
@@ -1,23 +1,22 @@
 /** JS */
+const isPlayed = Boolean(localStorage.getItem("is-played"));
 
 window.addEventListener("load", () => {
   const video = document.querySelector("video");
   const btnPlay = document.querySelector(".btn-play");
 
-  const isPlayed = localStorage.getItem("is-played");
-
-  if (isPlayed) {
-    btnPlay.disabled = true;
-  }
-
   document.addEventListener("contextmenu", event => event.preventDefault());
 
-  btnPlay.addEventListener("click", () => {
-    if (isPlayed) {
-      return;
-    }
-
-    video.play();
-    localStorage.setItem("is-played", true);
-  });
+  btnPlay.disabled = isPlayed;
+  btnPlay.addEventListener("click", playVideo.bind(null, btnPlay, video));
 });
+
+function playVideo(btnPlay, video) {
+  if (isPlayed) {
+    return;
+  }
+
+  video.play();
+  btnPlay.disabled = true;
+  localStorage.setItem("is-played", true);
+}
