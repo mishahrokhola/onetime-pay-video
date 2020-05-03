5cc40633b91a7c4288d29760f366b61405c785d6
Misha Hrokhola <m.hrokhola@tenantcloud.com>
2020-05-03 11:10:15 +0300

    add video one time tun code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log(11);
+/** JS */
+
+window.addEventListener("load", () => {
+  const video = document.querySelector("video");
+  const btnPlay = document.querySelector(".btn-play");
+
+  const isPlayed = localStorage.getItem("is-played");
+
+  if (isPlayed) {
+    btnPlay.disabled = true;
+  }
+
+  btnPlay.addEventListener("click", () => {
+    if (isPlayed) {
+      return;
+    }
+
+    video.play();
+    localStorage.setItem("is-played", true);
+  });
+});
